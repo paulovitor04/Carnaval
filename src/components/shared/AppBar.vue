091a717e37447fe6c9b3cc439e1e33fa8ab701e9
TedMartins <ted.freela@gmail.com>
2022-03-19 11:54:59 -0300

    Envio do AppBar

---
+++ src/components/shared/AppBar.vue
@@ -0,0 +1,71 @@
+<template>
+ <div class="app-bar">
+    <v-app-bar
+      app 
+      color="deep-purple"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Carnaval</v-toolbar-title>
+      <v-spacer></v-spacer><span class="text-right mr-6"><ThemeChanger /></span>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+
+          <router-link to="/">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          </router-link>
+
+          <router-link to="/about">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account-tie</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+          </router-link>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+  </div>
+</template>
+
+<script>
+import ThemeChanger from '@/components/shared/ThemeChanger.vue'
+  export default {
+    name: 'AppBar',
+    components: {
+    ThemeChanger,
+  },
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+  }
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/shared/ThemeChanger.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-tooltip v-if="!$vuetify.theme.dark" left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn v-on="on" v-bind="attrs" color="transparent" small fab @click="darkMode">
+            <v-icon>mdi-moon-waxing-crescent</v-icon>
+          </v-btn>
+        </template>
+        <span>Lights Off</span>
+      </v-tooltip>
+
+      <v-tooltip v-else left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn v-on="on" v-bind="attrs" color="transparent" small fab @click="darkMode">
+            <v-icon>mdi-white-balance-sunny</v-icon>
+          </v-btn>
+        </template>
+        <span>Lights On</span>
+      </v-tooltip>
+</template>
+
+
+<script>
+export default {
+  name: 'ThemeChanger',
+  data: () => ({
+      //
+  }),
+  methods: {
+  darkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    }
+  }
+}
+</script>
