6b78950a93fa0d988a0951ababb890f889d68997
Paulo Vitor de Oliveira <paulovitordeoliveira4@gmail.com>
2022-03-19 16:34:16 -0300

    edição pagina home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
+<div>
   <div class="home">
+    <img src="https://images.pexels.com/photos/268481/pexels-photo-268481.jpeg?cs=srgb&dl=pexels-pixabay-268481.jpg&fm=jpg">
+    <p>Mais um ano se passou e não conseguimos estar juntinhos na rua do jeito que a gente gosta, mas isso não quer dizer que 
+      você precisa abandonar sua (ou suas) escola de samba favorita.  Nosso web-site serve para te deixar perto daquilo que 
+      você ama, sem sair de casa. Acompanhe nossos ensaios e também nossas futuras apresentações que estão voltando.  
+      Não esqueça de deixar seu comentário, é muito importante para nós.</p>
   </div>
+</div>
 </template>
 
 <script>
@@ -14,3 +21,22 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.home img {
+  margin: 25px;
+  width: 50%;
+  height: 50%;
+  border-radius: 5px;
+  box-shadow: 5px 5px 3px rgba(0,0,0,0.5);
+  }
+
+ div {
+   text-align: center;
+ } 
+
+ p {
+   text-align: justify;
+   margin: 15px 50px;
+ }
+</style>
