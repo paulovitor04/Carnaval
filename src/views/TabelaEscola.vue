05211e5542726da22263484e4e1029dacba511d3
Paulo Vitor de Oliveira <paulovitordeoliveira4@gmail.com>
2022-03-19 14:43:55 -0300

    editação da pagina TabelaEscola

--- src/views/TabelaEscola.vue
+++ src/views/TabelaEscola.vue
@@ -1,13 +1,46 @@
 <template>
-  <div></div>
+<v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th colspan="2" class="text-left">Escola</th>
+          <th class="text-right">Cidade</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(escola, index) of listaEscolas" :key="escola.id">
+          <td class="nao">{{ index + 1 }}</td>          
+          <td> {{ escola.name }}</td>
+          <td class="text-right"> {{ escola.address }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+
+  
 </template>
 
 <script>
 export default {
-    
+    name: 'ListaEscolas',
+  data() {
+    return {
+      listaEscolas: []
+    }
+  },
+  created() {
+    fetch('https://it3-hbn-default-rtdb.firebaseio.com/carnaval.json')
+      .then(res => res.json())
+      .then(json => {
+        this.listaEscolas = json;
+    });
+  }
 }
 </script>
 
 <style scoped>
+.nao {
+    display: none;
+}
 
 </style>
